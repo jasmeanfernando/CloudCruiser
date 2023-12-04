ee724f9fb27bbe2c484191901182d31eb5606156
Jasmean Fernando <jasmeanfernando@gmail.com>
2023-12-03 21:28:58 -0500

    initializing repo

---
+++ CloudCruiser/src/main/java/com/cs336/pkg/ApplicationDB.java
@@ -0,0 +1,57 @@
+package com.cs336.pkg;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ApplicationDB
+{
+	public ApplicationDB()
+	{
+	}
+
+	public Connection getConnection()
+	{
+		//Create a connection string.
+		String connectionUrl = "jdbc:mysql://localhost:3306/BarBeerDrinkerSample";
+		Connection connection = null;
+		
+		try {
+			//Load JDBC driver--The interface standardizing the connection procedure.
+			//Look at WEB-INF\lib for a mysql connector jar file, otherwise it fails.
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			//Create a connection to your DB.
+			connection = DriverManager.getConnection(connectionUrl,"root", "root");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return connection;
+	}
+	
+	public void closeConnection(Connection connection)
+	{
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		ApplicationDB dao = new ApplicationDB();
+		Connection connection = dao.getConnection();
+		
+		System.out.println(connection);		
+		dao.closeConnection(connection);
+	}
+}
